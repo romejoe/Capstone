b79669de5a402efc90f31101641a5964a36ad7dd
Joey <jsromejoe@gmail.com>
2014-01-31 18:45:40 -0500

    started work on assembler

---
+++ Compiler/assembler.c
@@ -0,0 +1,12 @@
+#include "assembler.h"
+
+enum 
+
+
+
+void AssembleProgram(struct Program *program, void **target, int *targetLength){
+
+
+
+
+}
---
+++ Compiler/assembler.h
@@ -0,0 +1,8 @@
+#include "Utils/program.h"
+
+#ifndef ___ASSEMBLER_H___
+#define___ASSEMBLER_H___
+
+void AssembleProgram(struct Program *program, void **target, int *targetLength);
+
+#endif

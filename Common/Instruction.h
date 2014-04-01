76dc619251be873c975b08cbe6fac09e7824c4c5
Joey <stantoje@sunyit.edu>
2014-04-01 15:51:01 -0400

    added ability to allocate variables removed unused "program" and assembler code

--- Common/Instruction.h
+++ Common/Instruction.h
@@ -41,35 +41,18 @@ enum instruction_type {
 	iPOW,
 	iIPUSH,
 	iFPUSH,
+	iVPUSH,
+	iVPOP,
+	iVALLOC,
+	iVDALLOC,
 	iPRINT
 };
-
-#define __REGISTER_COUNT 14
-
-
-struct paramOption {
-	int location: 7; /* 2^4 = 16 possible locations */
-	/* 0: raw data */
-	/* 1: memory location */
-	int dereferenceLocation: 1;
-	unsigned char size:8; /*size in bytes of the following data*/
-
-};
-#define ParamOptionSize 2
 
 struct instruction {
 	char opType; /*allows for 2^8 (256) commands*/
 };
 
-struct complete_instruction {
-	struct instruction instruct;
-	char optionCount;
-	struct paramOption *options;
-};
 
 struct instruction new_instruction(enum instruction_type);
-int getParamCountForInstruction(struct instruction instruct);
-
-struct complete_instruction *new_complete_instruction(enum instruction_type);
 
 #endif
